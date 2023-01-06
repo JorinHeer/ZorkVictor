5fe2ddcb3b3d8810d547419beb829b00fa44ff59
Jorin Heer <jorin.heer@gmail.com>
2023-01-06 10:04:30 +0100

    upload for victor

---
+++ src/ch/bbw/na/Zork2Game.java
@@ -0,0 +1,10 @@
+package ch.bbw.na;
+
+public class Zork2Game {
+
+    public static void main(String[] args) {
+	// write your code here
+        Game Hello = new Game();
+        Hello.play();
+    }
+}
